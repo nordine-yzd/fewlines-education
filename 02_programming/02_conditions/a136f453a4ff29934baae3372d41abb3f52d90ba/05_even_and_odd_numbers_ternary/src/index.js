c9b10e0866d636773db4c5973075bfe272503f0b
nordine-yzd <nordine-elyazidi@hotmail.fr>
2022-01-12 17:03:49 +0100

    second day of programming with conditional notion

---
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/05_even_and_odd_numbers_ternary/src/index.js
@@ -0,0 +1,5 @@
+let numberToCheck = 18;
+
+// transform (or better, re do) the code of your previous exercise using a ternary operator.
+numberToCheck % 2 === 0 ? console.log("The number is even") :   console.log("The number is odd");
+
---
+++ 02_programming/02_conditions/a136f453a4ff29934baae3372d41abb3f52d90ba/06_conditional_statement_drink/src/index.js
@@ -0,0 +1,21 @@
+// > Frieda and Francis walk on the street. Frieda is 22 and Francis is 17.
+
+// Create two objects, `frieda`, `francis` with the fields :
+//   `age` (an integer) and `name`, (obviously a string)
+
+const frieda = {
+  age: 22,
+  name: "Frieda",
+};
+
+const francis = {
+  age: 17,
+  name: "Francis",
+};
+
+// Create two variables `canFriedaDrinkAlcohol` and `canFrancisDrinkAlcohol` to answer,
+// respectively, to the question "Can he / she drink alcohol?".
+c
+const canFriedaDrinkAlcohol = frieda.age >= 18 ? true : false;
+
+const canFrancisDrinkAlcohol = francis.age >= 18 ? true : false;
