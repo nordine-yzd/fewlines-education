ee139bd0135dec8b5fb41984c41af8fdc1bafe26
nordine-yzd <nordine-elyazidi@hotmail.fr>
2022-01-19 10:00:58 +0100

    99 bottles of beer

--- 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/04_99_bottles_of_beer/src/index.js
+++ 02_programming/06_loops/89d8ffe9d1edbe08b46af9bfaeb92045680be1bd/04_99_bottles_of_beer/src/index.js
@@ -1,29 +1,28 @@
 function sing() {
-  // code here
   let arr = [];
-  for (let i = 99; i > 0; i--) {
+  for (let i = 99; i >= 0; i--) {
     switch (i) {
+      case 0:
+        arr.push("No more bottles of beer on the wall, no more bottles of beer.");
+        arr.push("Go to the store and buy some more, 99 bottles of beer on the wall.");
+        break;
       case 1:
-        arr.push(
-          `No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n`,
-        );
+        arr.push("1 bottle of beer on the wall, 1 bottle of beer.");
+        arr.push("Take one down and pass it around, no more bottles of beer on the wall.");
         break;
       case 2:
-        arr.push(
-          `1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n`,
-        );
+        arr.push("2 bottle of beer on the wall, 2 bottle of beer.");
+        arr.push("Take one down and pass it around, 1 bottle of beer on the wall.");
         break;
       default:
-        arr.push(
-          `${i} bottles of beer on the wall, ${i} bottles of beer. \n Take one down and pass it around, ${
-            i - 1
-          } bottles of beer on the wall.\n`,
-        );
+        arr.push(`${i} bottles of beer on the wall, ${i} bottles of beer.`);
+        arr.push(`Take one down and pass it around, ${i - 1} bottles of beer on the wall.`);
         break;
     }
   }
   return arr;
 }
+console.log(sing());
 
 // Do not remove the following line, it is for tests
 module.exports = sing;
