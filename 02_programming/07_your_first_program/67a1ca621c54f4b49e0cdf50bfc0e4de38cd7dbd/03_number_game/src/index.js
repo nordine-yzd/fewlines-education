e5fe443a99386915f061fcf3d10d5f1b216ad0e3
nordine-yzd <nordine-elyazidi@hotmail.fr>
2022-01-20 09:49:12 +0100

    exo 3 number game debut de piste qui fonctionne

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/index.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/index.js
@@ -1,6 +1,7 @@
 const readline = require("readline");
 
 const numberGame = require("./numberGame");
+
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
@@ -1,5 +1,42 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 function numberGame(reader, min = 1, max = 100) {
-  // code here
+  console.log("entree dans la fonction");
+  //random entre 1 compris et 100 compris
+  const random = Math.floor(Math.random() * 100) + 1;
+  console.log("Welcome !\nYou have to find the right number, between 1 and 100!\nGood luck!!\n ");
+  console.log(random);
+  reader.question("Enter a number\n", (number) => {
+    if (number == random) {
+      console.log("You won!");
+      reader.close();
+    } else if (number > random && number < 100) {
+      console.log("Too low");
+      reader.close();
+      numberGame(reader, min, max);
+    }
+    // else if (number > random && number === 100) {
+    //   console.log("Too high");
+    //   reader.close();
+    // } else if (typeof number === "string") {
+    //   console.log("This was not a number");
+    //   reader.close();
+    // } else if (number < min || number > max) {
+    //   console.log("The number is between 1 and 100");
+    //   reader.close();
+    // } else if (number === random) {
+    //   console.log("You won!");
+    //   validation = true;
+    //   reader.close();
+    // }
+  });
 }
+
+numberGame(reader);
 
 module.exports = numberGame;
