a3a1db8bf43b219f73678c82a0a814345e0ec94d
nordine-yzd <nordine-elyazidi@hotmail.fr>
2022-01-24 08:57:30 +0100

    exo du weekend

--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/03_choose_operation/src/chooseOperation.js
@@ -1,39 +1,42 @@
 function addition(a, b) {
   // Code here
-  const result =  a + b ;
-  return console.log(result);
+  const result = a + b;
+  return result;
 }
-addition(1, 2);
+// addition(1, 2);
 function substraction(a, b) {
   // Code here
-  const result =  a - b ;
-  return console.log(result);
+  const result = a - b;
+  return result;
 }
-substraction(10, 2);
+// substraction(10, 2);
 
 function multiplication(a, b) {
   // Code here
-  const result =  a * b ;
-  return console.log(result);
+  const result = a * b;
+  return result;
 }
 
 function division(a, b) {
   // Code here
-  const result =  a / b ;
-  return console.log(result);
+  const result = a / b;
+  return result;
 }
 
-function chooseOperation(int1, int2,operation) {
-  const getFonctions = fonctions[operation] ;
-  operation(getFonctions);
+function chooseOperation(int1, int2, operation) {
+  if (typeof operation === "function" && Number.isInteger(int1) === true && Number.isInteger(int2) === true) {
+    return operation(int1, int2);
+  } else {
+    throw new Error("expected chooseOperation || addition || substraction || multiplication || division ");
+  }
 }
+const rr = addition(1, 3);
+console.log(rr);
+console.log(Number.isInteger(rr));
+// const result = chooseOperation(3, 2, addition);
 
-
-
-
-chooseOperation(1,1,addition);
-
-
+// console.log(result);
+// console.log(Number.isInteger(1 / 2));
 
 // Leave the line below for tests to work
 module.exports = { chooseOperation, addition, substraction, multiplication, division };
--- 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
+++ 02_programming/04_callbacks/e25484c82abd17b6a9c75aba5404ceb1d507d74d/06_recursion/src/recursion.js
@@ -1,6 +1,11 @@
-const findPosition = () => {
+const findPosition = (array,string,number) => {
   // Code here
 };
+
+const result = findPosition(["Fenn", "Florian", "Martin", "Louis", "Thomas"], "Martin", 0);
+
+console.log(result);
+
 
 // Leave line below for tests to work properly
 module.exports = findPosition;
