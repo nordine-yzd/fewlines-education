68e9b7b8faa566c0cd6a295a7d5956faf29cbedc
nordine-yzd <nordine-elyazidi@hotmail.fr>
2022-01-27 09:45:46 +0100

    exo game number

--- 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
+++ 02_programming/07_your_first_program/67a1ca621c54f4b49e0cdf50bfc0e4de38cd7dbd/03_number_game/src/numberGame.js
@@ -6,34 +6,27 @@ const reader = readline.createInterface({
 });
 
 function numberGame(reader, min = 1, max = 100) {
-  console.log("entree dans la fonction");
   //random entre 1 compris et 100 compris
-  const random = Math.floor(Math.random() * 100) + 1;
+  const random = Math.floor(Math.random() * max) + min;
   console.log("Welcome !\nYou have to find the right number, between 1 and 100!\nGood luck!!\n ");
   console.log(random);
   reader.question("Enter a number\n", (number) => {
-    if (number == random) {
-      console.log("You won!");
-      reader.close();
-    } else if (number > random && number < 100) {
-      console.log("Too low");
-      reader.close();
-      numberGame(reader, min, max);
+    if (Number.isInteger(number) === true) {
+      if (number == random) {
+        console.log("You won!");
+      }
+      if (number > random && number < 100) {
+        console.log("Too high");
+        reader.close();
+        numberGame(reader, min, max);
+      }
+      if (number < random && number > 0) {
+        console.log("Too low");
+        numberGame(reader, min, max);
+      }
+    } else {
+      console.log("This was not a number");
     }
-    // else if (number > random && number === 100) {
-    //   console.log("Too high");
-    //   reader.close();
-    // } else if (typeof number === "string") {
-    //   console.log("This was not a number");
-    //   reader.close();
-    // } else if (number < min || number > max) {
-    //   console.log("The number is between 1 and 100");
-    //   reader.close();
-    // } else if (number === random) {
-    //   console.log("You won!");
-    //   validation = true;
-    //   reader.close();
-    // }
   });
 }
 
