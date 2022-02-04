bea10b9383572b9e157f5d20da6e740e5f0b8ac6
nordine-yzd <nordine-elyazidi@hotmail.fr>
2022-02-04 13:34:51 +0100

    exo5

--- 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/05_coffee_recipes_with_object/src/index.test.ts
+++ 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/05_coffee_recipes_with_object/src/index.test.ts
@@ -28,34 +28,31 @@ const expectedCoffeeRecipes: CoffeeRecipe[] = [
   },
 ];
 
-describe("#recipeMaker", () => {
-  describe("#coffeeRecipes", () => {
-    test("", () => {
+describe("some test from const recipeMaker", () => {
+  describe("#gruope test of const recipeMaker ", () => {
+    test("check if cofeeRecipe equal to expectedCoffeeRecipes Array ", () => {
       expect.assertions(1);
       expect(expectedCoffeeRecipes).toEqual(recipeMaker.coffeeRecipes);
     });
   });
 
-  describe("#giveRecipe", () => {
+  describe("group test of giveRecipe function", () => {
     test("", () => {
       expect.assertions(2);
       expect(typeof recipeMaker.giveRecipe).toEqual("function");
       expect(recipeMaker.giveRecipe.length).toEqual(1);
     });
-
     test("it should return a string", () => {
       expect.assertions(2);
       expect(typeof recipeMaker.giveRecipe("cappuccino")).toEqual("string");
       expect(typeof recipeMaker.giveRecipe("cola")).toEqual("string");
     });
-
     test("test text sample", () => {
       expect.assertions(3);
       expect(recipeMaker.giveRecipe("LATTE")).toEqual("Sorry, LATTE does not exist on our list.");
       expect(recipeMaker.giveRecipe("latt")).toEqual("Sorry, latt does not exist on our list.");
       expect(recipeMaker.giveRecipe("cola")).toEqual("Sorry, cola does not exist on our list.");
     });
-
     test("dont forget to give a description LOL", () => {
       expect.assertions(5);
       recipeMaker.coffeeRecipes.forEach((coffee) => {
--- 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/05_coffee_recipes_with_object/src/index.ts
+++ 02_programming/18_unit_testing_1/c25f5cc60d074f354fcd750f905fda0317f3adbf/05_coffee_recipes_with_object/src/index.ts
@@ -1,3 +1,48 @@
 export const recipeMaker = {
   //code here
+  coffeeRecipes: [
+    {
+      name: "espresso",
+      ingredients: ["espresso"],
+    },
+    {
+      name: "macchiato",
+      ingredients: ["espresso", "milk foam"],
+    },
+    {
+      name: "latte",
+      ingredients: ["espresso", "milk foam", "steamed milk"],
+    },
+    {
+      name: "cappuccino",
+      ingredients: ["espresso", "milk foam", "steamed milk", "chocolate"],
+    },
+    {
+      name: "americano",
+      ingredients: ["espresso", "water"],
+    },
+  ],
+  giveRecipe: function giveRecipe(dataReceved: string): string {
+    let result: string = "empty";
+
+    for (let i = 0; i < recipeMaker.coffeeRecipes.length; i++) {
+      // console.log(i);
+      // console.log(recipeMaker[i].name);
+      if (recipeMaker.coffeeRecipes[i].name === dataReceved) {
+        result = `${recipeMaker.coffeeRecipes[i].name} ingredients:\n- ${recipeMaker.coffeeRecipes[i].ingredients.join(
+          ",\n- ",
+        )}`;
+      }
+    }
+    if (result === "empty") {
+      result = `Sorry, ${dataReceved} does not exist on our list.`;
+    }
+    return result;
+  },
 };
+
+// console.log(recipeMaker.coffeeRecipes);
+// recipeMaker.giveRecipe();
+
+// const re = giveRecipe("espresso");
+// console.log(re);
