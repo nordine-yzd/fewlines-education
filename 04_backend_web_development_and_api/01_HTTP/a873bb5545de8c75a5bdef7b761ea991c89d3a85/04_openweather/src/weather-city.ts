d309ac310a80783f1d22644f348431af54d2cc5c
nordine-yzd <nordine-elyazidi@hotmail.fr>
2022-02-14 17:14:33 +0100

    modification

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-city.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-city.ts
@@ -3,7 +3,6 @@ import "dotenv/config";
 
 function weatherByCity(city: string): void {
   // code the function here
-  // e00116f2bbce155d56ea3d3f20680b2c;
   request(
     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}`,
     (error, body) => {
