c5def91c8728578bf47594c59ed139d77e1e6950
nordine-yzd <nordine-elyazidi@hotmail.fr>
2022-02-15 09:17:42 +0100

    exo xheater

--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-city.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-city.ts
@@ -4,13 +4,13 @@ import "dotenv/config";
 function weatherByCity(city: string): void {
   // code the function here
   request(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}`,
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`,
     (error, body) => {
       if (error) {
         console.error(error);
       } else {
-        // const json = JSON.parse(body);
-        console.log(body);
+        const json = JSON.parse(body);
+        console.log(Math.round(json.main.temp) + "°C");
       }
     },
   );
--- 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-forecast.ts
+++ 04_backend_web_development_and_api/01_HTTP/a873bb5545de8c75a5bdef7b761ea991c89d3a85/04_openweather/src/weather-forecast.ts
@@ -1,16 +1,28 @@
 import request from "@fewlines-education/request";
 import "dotenv/config";
 
-function weatherByZipcode(zipcode, countryCode) {
+function weatherByZipcode(zipcode: string, countryCode: string) {
   // code the function here
-}
+  request(
+    `http://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},${countryCode}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`,
+    (error, body) => {
+      if (error) {
+        console.error(error);
+      } else {
+        // console.log({ body });
 
-function weatherByLatitudeAndLongitude(latitude, longitude) {
-  // code the function here
+        const json = JSON.parse(body);
+        // console.log(Math.round(json) + "°C");
+        console.log(json);
+      }
+    },
+  );
 }
+weatherByZipcode("59000", "fr");
+
+// function weatherByLatitudeAndLongitude(latitude, longitude) {
+//   // code the function here
+// }
 
 // leave lines below for tests to work properly
-export {
-  weatherByZipcode,
-  weatherByLatitudeAndLongitude
-}
+export { weatherByZipcode };
